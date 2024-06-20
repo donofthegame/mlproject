0bae1f3f123f8c6e845fc4ba0ba86a149876ff54
donofthegame <donofthetechgame>
2024-06-20 18:36:45 -0400

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-import logger  # import logger.py into this file
+from src.logger import logging  # import logger.py into this file
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
